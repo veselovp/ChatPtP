7c65f1127e435f2da438241d77fae0c863f5e8e1
Pavel Vslv <114096039+veselovp@users.noreply.github.com>
2023-04-19 17:30:39 +0100

    finish registration

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBmsMfpbVnCVuFBGPcDa4rIE18VnKlsYXY',
@@ -12,3 +14,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth()
+export const storage = getStorage()
+export const db = getFirestore()
